eeb9e79de0cbbf5e74a6e3d913d2685711328617
Matthew Stefanovic <matthew@stefanvoic.us>
2022-05-11 14:14:46 -0400

    added minor changes to script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
+# *** Table of Contents ***
+# * 1. 
+# * 
+
 # ------------------ Setup DNF -----------------
 
 dnf_setup () {
+    # Make default dnf option yes 
+    echo "defultyes=True" >> /etc/dnf/dnf.conf
     # Updateing and Upgrading Fedora 
-
+    dnf -y update
+    dnf -y upgrade
 
     # Adding Fusion Repos (Non-Foss Software) 
-
+    dnf -y install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+    dnf -y install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
     # Adding _____ Repos
 }
@@ -16,38 +26,49 @@ install_dnf_apps () {
     # --------- Adding 3rd party repos ---------
 
     # Brave Browser
-    dnf install dnf-plugins-core
-    dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+    dnf -y install dnf-plugins-core
+    dnf -y config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
     rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
+    # ------------------------------------------
 
-    dnfApps = (
-	"vim" 			# vim
-	"nvim" 			# Neovim
-	'zsh'			# zsh
-	'htop'			# hop
-	'gnome-system-monitor'	# Gnome System Monitor
-	'gparted'		# Gparted
-	'alacritty'		# Alacritty
-	'virt-manager'		# Virtual Machine Manager
-	'virt-viewer'		# Linux Virtaul Machine 
-	'qemu-kvm'		# Linux Virtual Machine
-	'python-virinst'	# Linux Virtual Machine
-	'ranger'		# Ranger File Manager
-	'solaar'		# Solaar
-	'python3'		# Python 3
-	'java-latest-openjdk.x86_64' # Latest verson of openJDK
-	'cmake'			# CMake
-	'steam'			# Steam
-	'brave-browser'		# Brave Browser
+    dnfApps=(
+	"neovim" 			# Neovim
+	"vim"				# Vim
+	"zsh"			# zsh
+	"htop"			# hop
+	"gnome-system-monitor"	# Gnome System Monitor
+	"gparted"		# Gparted
+	"alacritty"		# Alacritty
+	"virt-manager"		# Virtual Machine Manager
+	"virt-viewer"		# Linux Virtaul Machine 
+	"qemu-kvm"		# Linux Virtual Machine
+	"ranger"		# Ranger File Manager
+	"solaar"		# Solaar
+	"python3"		# Python 3
+	"java-latest-openjdk.x86_64" # Latest verson of openJDK
+	"cmake"			# CMake
+	"steam"			# Steam
+	"brave-browser"		# Brave Browser
     ) 
+	
+	allAppsDNF=""
+    for app in ${dnfApps[@]}; do
+        allAppsDNF+="${app} "
+    done
+	
+	echo "$allAppsDNF"
+	dnf -y install $allAppsDNF 
 } 
 
 # --------------- Flatpak Apps -----------------
 
 install_flatpak_apps () {
-    flatpakApps = (
-        "com.spotify.Client" 			# Spotify
+	# Adds flathub repo
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    
+    flatpakApps=(
+    "com.spotify.Client" 			# Spotify
 	"org.blender.Blender" 			# Blender
 	"com.github.Eloston.UngoogledChromium" 	# Ungoogled Chromium
 	"org.freecadweb.FreeCAD" 		# FreeCAD		
@@ -57,7 +78,6 @@ install_flatpak_apps () {
 	"net.ankiweb.Anki"			# Anki
 	"com.github.marktext.marktext" 		# MarkText
 	"com.github.tchx84.Flatseal"		# Flatseal
-	"org.libreoffice.LibreOffice"		# LibreOffice
 	"org.onlyoffice.desktopeditors"		# OnlyOffice
 	"com.microsoft.Teams"			# Microsoft Teams
 	"com.obsproject.Studio"			# OBS Studio
@@ -76,11 +96,31 @@ install_flatpak_apps () {
 	"com.github.xournalpp.xournalpp"	# Xournal++
 	"md.obsidian.Obsidian" 			# Obsidian
 	"com.discordapp.Discord"		# Discord
-	'org.gnome.DejaDup'			# Déjà Dup Backups
+	"org.gnome.DejaDup"			# Déjà Dup Backups
     )
 
-    gnomeFlatpaks = (
+    gnomeFlatpaks=(
 	"org.gnome.Extensions" # Gnome Extentions    
 
     )
+
+	allAppsFlatpak=""
+	for app in ${flatpakApps[@]}; do 
+		allAppsFlatpak+="${app} "
+	done
+
+	echo "$allAppsFlatpak"	
+	flatpak install flathub -y $allAppsFlatpak
 } 
+
+# ----------------- What to do -----------------
+
+echo "What Distro are you running? (1. Fedora, 2. Ubuntu, or 3. Arch)"
+read DISTRO
+
+if (( $DISTRO == 1 )); then
+	dnf_setup
+	install_dnf_apps 
+	install_flatpak_apps
+fi 
+
